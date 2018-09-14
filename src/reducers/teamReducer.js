b90f9d23276f36b3b5a5935553e7f291f1f75292
Piotrek <piotr.pietruszka@o2.pl>
2018-09-15 01:58:43 +0200

    add team members

--- src/reducers/teamReducer.js
+++ src/reducers/teamReducer.js
@@ -4,21 +4,27 @@ const SET_PROFILE = 'SET_PR2OFILE';
 const initialState = [
   {
     name: 'Jan Kowalski',
+    email: 'a-email@example.com',
   },
   {
     name: 'Adrian Nowak',
+    email: 'a-emawqeqweqwil@example.com',
   },
   {
     name: 'Jan Kowalski',
+    email: 'a-emawqeqweqwil@exawqeqweweqmple.com',
   },
   {
     name: 'Adrian Nowak',
+    email: 'a-emfsdsdfeqwil@exawqeqweweqmple.com',
   },
   {
     name: 'Jan Kowalski',
+    email: 'a-emawqeqweqwil@ex23132mple.com',
   },
   {
     name: 'Adrian Nowak',
+    email: 'a-emawqeqweqwil@exawdfsle.com',
   }
 ];
 
